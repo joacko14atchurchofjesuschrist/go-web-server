2c3e0036d264bb86e838f1af24aa6a82a01250b2
Joaquín Vázquez <joacko14@churchofjesuschrist.com>
2025-05-05 18:54:14 -0600

    Implement Gorilla Mux for routing and add profile endpoint

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example.com/m
 
 go 1.24.2
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- server.go
+++ server.go
@@ -3,15 +3,30 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!, you´ve requested the path: %s", r.URL.Path)
+	// Create a new router
+	r := mux.NewRouter()
+
+	// Define a route for the profile URL
+	r.HandleFunc("/profile", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello Joaquín!, you´ve requested your profile data")
+	})
+
+	// Define a route for the profile URL with a variable
+	r.HandleFunc("/profile/{name}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		name := vars["name"]
+		fmt.Fprintf(w, "Hello %s!, you´ve requested your profile data", name)
 	})
 
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static", fs))
-	http.ListenAndServe(":80", nil)
+
+	// Start the server on port 80 and use the router
+	http.ListenAndServe(":80", r)
 }
